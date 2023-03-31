c571652892e2f26d9a4d213991bc5f01aaea4e53
WermersonMacs <whermersonm@gmail.com>
2023-03-31 01:28:50 -0300

    EXERCICIO 03 RESOLVIDO

--- Exercicio03/app/src/main/java/Exercicio03/App.java
+++ Exercicio03/app/src/main/java/Exercicio03/App.java
@@ -1,5 +1,8 @@
 /*
- * Faça um algoritmo que receba o preço de custo e o preço de venda de 40 produtos. Mostre como resultado se houve lucro, prejuízo ou empate para cada produto. Informe o valor de custo de cada produto, o valor de venda de cada produto, amédia de preço de custo e do preço de venda;
+ * Faça um algoritmo que receba o preço de custo e o preço de venda de 40 produtos. 
+ *Mostre como resultado se houve lucro, prejuízo ou empate para cada produto. 
+ *Informe o valor de custo de cada produto, o valor de venda de cada produto, 
+ *a média de preço de custo e do preço de venda;
 */
 package Exercicio03;
 
@@ -9,6 +12,46 @@ public class App {
     
 
     public static void main(String[] args) {
+     
+    
+        String nomeProduto;
+        float precoCusto;
+        float precoVenda;
         
+        float mediaCusto = 0.0f;
+        float mediaVenda = 0.0f;
+        
+        Scanner scan = new Scanner(System.in);
+        int i = 0;
+        for (; i<5; i++){
+            
+            System.out.println("Digite o Nome do Produto:");
+            nomeProduto = scan.next();
+            
+            System.out.println("Digite o Custo do Produto:");
+            precoCusto = scan.nextFloat();
+            
+            System.out.println("Digite o Preço do Produto:");
+            precoVenda = scan.nextFloat();
+            
+            mediaCusto = mediaCusto + precoCusto;
+            mediaVenda = mediaVenda + precoVenda;
+            
+            if (precoCusto == precoVenda){
+                System.out.println(" Você não está no prejuízo e o valor está EMPATE.");
+                
+            } else {
+                if (precoVenda > precoCusto){
+                  System.out.println(" Você Obitever um Lucro de "+ ( precoVenda - precoCusto));
+                    
+            } else{ 
+                    System.out.println(" Você está no Prejuízo de " + (precoVenda - precoCusto));
+            }
+                 
+                }
+            System.out.println(nomeProduto + " Preço do Custo = " + precoCusto + ", Preço da Venda: " + precoVenda); 
+        } 
+        System.out.println("A média do preço de custo é de: " + (mediaCusto / i));
+        System.out.println("A média do Preço de venda é de: "+ (mediaVenda / i));
     }
 }
