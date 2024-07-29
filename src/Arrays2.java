93a2ba2cdaa410d0eaae607cdeb8fd8e783095ce
Wermerson Macedo <wermersonm@gmail.com>
2024-07-29 20:00:40 -0300

    Tipo de Arrays2/String

--- src/Arrays2.java
+++ src/Arrays2.java
@@ -1,2 +1,12 @@
 public class Arrays2 {
+    public static void main(String[] args) {
+        String[] nomes = new String[4];
+        nomes[0] = "Naruto";
+        nomes[1] = "Dragon Ball";
+        nomes[2] = "Pokemon";
+        nomes[3] = "Dragon Ball Super";
+        for (int i =0; i<nomes.length;i++){
+            System.out.println(nomes[i]);
+        }
+    }
 }
